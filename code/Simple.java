fdbe6f9f8d04f6e3011412b5986b8087867c9173
YogitaMD <yogitamd15@gmail.com>
2022-04-10 10:29:40 -0700

    first commit

---
+++ code/Simple.java
@@ -0,0 +1,8 @@
+//To run the code javac Simple.javac
+//java Simple
+package code;
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
